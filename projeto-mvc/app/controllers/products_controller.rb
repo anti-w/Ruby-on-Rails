884a768e0c80452f96dc0ffcca35742d86901392
Wendel Demetrio Bispo <wendel.bispo@dponet.com.br>
2023-04-06 11:05:42 -0300

    following steps until video 9 (not completed 9 yet)

---
+++ projeto-mvc/app/controllers/products_controller.rb
@@ -0,0 +1,43 @@
+class ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :edit]
+    
+    
+    def index
+        @products = Product.all
+    end
+
+    def show; end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to products_url
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit; end
+
+    def update
+        if @product.update(product_params)
+            redirect_to products_url
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def set_product
+        @product = Product.find(params[:id])
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :quantity, :price)
+    end
+end
