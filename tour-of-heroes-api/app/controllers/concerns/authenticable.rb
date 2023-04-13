8b106b4737d0ca7e90616a6e9eb4905c4da9dd3f
Wendel Demetrio Bispo <wendel.bispo@dponet.com.br>
2023-04-13 10:01:11 -0300

    install rubocop and configs to works fine

--- tour-of-heroes-api/app/controllers/concerns/authenticable.rb
+++ tour-of-heroes-api/app/controllers/concerns/authenticable.rb
@@ -1,15 +1,15 @@
 module Authenticable
-    private
+  private
 
-    def authenticate_with_token
-        @token ||= request.headers['Authorization']
+  def authenticate_with_token
+    @token ||= request.headers['Authorization']
 
-        unless valid_token?
-            render json: {erros: 'Você não tem autorização para essa operação.' } , status: :unauthorized
-        end
-    end
+    return if valid_token?
 
-    def valid_token? 
-        @token.present? && @token == Rails.application.credentials.token
-    end
+    render json: { erros: 'Você não tem autorização para essa operação.' }, status: :unauthorized
+  end
+
+  def valid_token?
+    @token.present? && @token == Rails.application.credentials.token
+  end
 end
