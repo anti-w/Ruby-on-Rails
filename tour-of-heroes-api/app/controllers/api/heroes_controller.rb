127a2669ddebae353120ab73603f03d54d9d2acc
Wendel Demetrio Bispo <wendel.bispo@dponet.com.br>
2023-04-13 10:06:40 -0300

    added token column on hero model

--- tour-of-heroes-api/app/controllers/api/heroes_controller.rb
+++ tour-of-heroes-api/app/controllers/api/heroes_controller.rb
@@ -1,8 +1,8 @@
 class Api::HeroesController < ApplicationController
   include Authenticable
 
-  before_action :authenticate_with_token, except: [:index, :show]
-  before_action :set_hero, only: [:show, :update, :destroy]
+  before_action :authenticate_with_token, except: %i[index show]
+  before_action :set_hero, only: %i[show update destroy]
 
   # GET /heroes
   def index
@@ -42,13 +42,14 @@ class Api::HeroesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_hero
-      @hero = Hero.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def hero_params
-      params.require(:hero).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_hero
+    @hero = Hero.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def hero_params
+    params.require(:hero).permit(:name)
+  end
 end
